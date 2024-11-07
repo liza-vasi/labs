5a8d182067d8af6dee2e8a3db123f8e7eca482d3
liza_vas <viki.sova@yandex.ru>
2024-11-07 12:38:23 +0700

    сделана 3 лабораторная работа

--- algoritm/lab3/lab3.cpp
+++ algoritm/lab3/lab3.cpp
@@ -1,74 +1,144 @@
-﻿#include  <iostream> 
-#include  <conio.h> 
-#include  <vector> 
-#include <ctime> 
+﻿#include <iostream>
+#include <vector>
+#include <random>
+#include <chrono>
+#include <fstream>
+#include <string>
+#include <conio.h>
+#include <cmath>
 
-void shellSort(std::vector<int>& arr) {
+void shellSort(std::vector<int>& arr, int gap) {
     int n = arr.size();
-    int gap = n / 2; 
-
-    while (gap > 0) {
-        for (int i = gap; i < n; i++) {
-            int temp = arr[i];
-            int j = i;
-
-            while (j >= gap && arr[j - gap] > temp) {
-                arr[j] = arr[j - gap];
-                j -= gap;
-            }
-            arr[j] = temp;
+    for (int i = gap; i < n; i++) {
+        int temp = arr[i];
+        int j;
+        for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+            arr[j] = arr[j - gap];
         }
-        gap /= 2; 
+        arr[j] = temp;
+    }
+}
+
+void shellSortWithHibbardGaps(std::vector<int>& arr) {
+    int n = arr.size();
+    std::vector<int> gaps;
+    for (int m = 1; (1 << m) - 1 < n; m++) {
+        gaps.push_back((1 << m) - 1); // 2^m - 1
+    }
+    for (int i = gaps.size() - 1; i >= 0; i--) {
+        shellSort(arr, gaps[i]);
+    }
+}
+
+void shellSortWithKnuthGaps(std::vector<int>& arr) {
+    int n = arr.size();
+    std::vector<int> gaps;
+    for (int gap = 1; gap < n; gap = 3 * gap + 1) {
+        gaps.push_back(gap);
+    }
+    for (int i = gaps.size() - 1; i >= 0; i--) {
+        shellSort(arr, gaps[i]);
+    }
+}
+
+void shellSortWithSedgwickGaps(std::vector<int>& arr) {
+    int n = arr.size();
+    std::vector<int> gaps;
+    for (int m = 0; ; m++) {
+        int gap;
+
+        if (m % 2 == 0) {
+            gap = 9 * (1 * pow(2, m / 2)) - 9; 
+        }
+        else {
+            gap = 8 * (1 * pow(2, (m + 1) / 2)) - 6;
+        }
+
+        if (gap >= n) break;
+        gaps.push_back(gap);
+    }
+    for (int i = gaps.size() - 1; i >= 0; i--) {
+        shellSort(arr, gaps[i]);
     }
 }
 
 bool isSorted(const std::vector<int>& arr) {
-    for (size_t i = 0; i < arr.size() - 1; ++i) {
-        if (arr[i] > arr[i + 1]) {
+    for (int i = 1; i < arr.size(); i++) {
+        if (arr[i] < arr[i - 1]) {
             return false;
         }
     }
     return true;
 }
-void randMas1(std::vector<int>& matr, int n) {
-    matr.resize(n);
-    for (int i = 0; i < n; i++) {
-        matr[i] = std::rand() % 21 - 10; 
-    }
-}
-void randMas2(std::vector<int>& matr, int n) {
-    matr.resize(n);
-    for (int i = 0; i < n; i++) {
-        matr[i] = std::rand() % 2001 - 1000;
-    }
-}
-void randMas3(std::vector<int>& matr, int n) {
-    matr.resize(n);
-    for (int i = 0; i < n; i++) {
-        matr[i] = std::rand() % 200001 - 100000;
-    }
-}
-int main() {
-    srand(static_cast<unsigned int>(time(0)));
-    setlocale(LC_ALL, "Russian");
-    // Исходный массив
-    int n;
-    std::vector<int> array;
-    for (int i = 0, n = 10000;i < 3;i++, n = n * 10) {
-        randMas1(array, n);
-        std::cout << "\n"<< "Количество элементов " << n << "\n";
-        std::cout << "Диапазон от -10 до 10" << "\n";;
-        unsigned int start_time = clock(); // начальное время
-        shellSort(array);
-        unsigned int end_time = clock(); // конечное время
-        unsigned int search_time = end_time - start_time;
-        std::cout << "Время работы (в миллисекундах) " << search_time << "\n";
 
-        if (isSorted(array)) {
-            std::cout << "Массив отсортирован корректно." << '\n' << '\n';
+std::vector<int> generateRandomArray(int size, int minValue, int maxValue) {
+    std::vector<int> arr(size);
+
+    
+    std::random_device rd; // получаем случайное число от устройства
+    std::mt19937 gen(rd()); // инициализация генератора
+    std::uniform_int_distribution<> dis(minValue, maxValue); // определяем распределение
+
+    for (int i = 0; i < size; ++i) {
+        arr[i] = dis(gen); // генерируем числа в диапазоне [minValue, maxValue]
+    }
+
+    return arr;
+}
+
+void writeArrayToFile(const std::vector<int>& arr, const std::string& filename) {
+    std::ofstream outFile(filename);
+    for (int i = 0; i < arr.size(); i++) {
+        outFile << arr[i] << "\n";
+    }
+}
+
+
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    const std::vector<int> sizes = { 10000, 100000, 1000000 };
+    const std::vector<std::pair<int, int>> ranges = { {-10, 10}, {-1000, 1000}, {-100000, 100000} };
+
+    
+    for (int i = 0; i < sizes.size(); i++) {
+        int size = sizes[i];
+        for (int j = 0; j < ranges.size(); j++) {
+            std::pair<int, int> range = ranges[j];
+            std::vector<int> arr = generateRandomArray(size, range.first, range.second);
+            std::string filename = "array_" + std::to_string(size) + "_" + std::to_string(range.first) + "_" + std::to_string(range.second) + ".txt";
+            writeArrayToFile(arr,filename);
         }
-        else {
-            std::cout << "Массив не отсортирован." << '\n' << '\n';
+    }
+
+    
+    for (size_t i = 0; i < sizes.size(); ++i) {
+        int size = sizes[i];
+        for (size_t j = 0; j < ranges.size(); ++j) {
+            std::pair<int, int> range = ranges[j];
+            std::vector<int> arrHibbard = generateRandomArray(size, range.first, range.second);
+            std::vector<int> arrKnuth = arrHibbard; 
+            std::vector<int> arrSedgwick = arrHibbard;
+            
+            unsigned int start_time1 = clock(); // начальное время
+            shellSortWithHibbardGaps(arrHibbard);
+            unsigned int end_time1 = clock(); // конечное время
+            unsigned int timeHibbard = end_time1 - start_time1;
+            std::cout << "Время работы Хиббард (в миллисекундах) " << timeHibbard << "\n";
+
+            unsigned int start_time2 = clock(); // начальное время
+            shellSortWithKnuthGaps(arrKnuth);
+            unsigned int end_time2 = clock(); // конечное время
+            unsigned int timeKnuth = end_time2 - start_time2;
+            std::cout << "Время работы Кнут (в миллисекундах) " << timeKnuth << "\n";
+
+            unsigned int start_time3 = clock(); // начальное время
+            shellSortWithSedgwickGaps(arrSedgwick);
+            unsigned int end_time3 = clock(); // конечное время
+            unsigned int timeSedgwick = end_time3 - start_time3;
+            std::cout << "Время работы Седвик (в миллисекундах) " << timeSedgwick << "\n";
+            
+
         }
     }
     _getch();
